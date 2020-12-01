9d633b9dd8b5fe0fcd67f12d17a14466c8dd92aa
jantiks <58886410+jantiks@users.noreply.github.com>
2020-12-01 14:57:21 +0400

    Initial Commit

---
+++ project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project7
+//
+//  Created by Tigran on 12/1/20.
+//  Copyright © 2020 Tigran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
